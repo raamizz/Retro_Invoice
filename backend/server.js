fbaa3e96ffd1647604915bb63dd7f4c5da642d10
raamizz <rameezebrahim@transpost.co>
2025-06-20 12:47:16 +0530

    Successfully confgured server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-const express = require("");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const pool = require("./config/database")
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+pool.connect()
+  .then(() => {
+    console.log("Connected to PostGre");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("PostGre connection failed:", err.message);
+  });
