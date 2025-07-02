37a8751ea8075dbea0e0b0e9b9a1bf0b43bcbc10
raamizz <rameezebrahim@transpost.co>
2025-07-02 13:01:05 +0530

    Building

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => (
+  <header>
+    <div className="bg-black flex justify-between items-center px-5 py-4">
+      <h1 className="text-white font-medium"> RETRO INVOICE</h1>
+      <div className="flex gap-2">
+        <button className="bg-green-700 hover:bg-green-500 text-white p-2 rounded w-full sm:w-auto">
+          Save
+        </button>
+        <button className="bg-gray-100 hover:bg-gray-200 text-gray-800 p-2 rounded w-full sm:w-auto">
+          Reset
+        </button>
+        <button className="bg-white border border-gray-200 text-gray-800 p-2 rounded w-full sm:w-auto">
+          LogOut
+        </button>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
