cd992bf6112567b8706dc02b515ac8f516684f5f
raamizz <rameezebrahim@transpost.co>
2025-07-02 13:07:56 +0530

    Formation added

--- frontend/src/components/Invoice.jsx
+++ frontend/src/components/Invoice.jsx
@@ -1,9 +1,24 @@
+import InvoiceForm from "./InvoiceForm";
+import AdditionalCostTable from "./AdditionalCostTable";
+import GSTDetailsTable from "./GSTDetailsTable";
+
+
 const Invoice = ()=>{
 
 
 
     return (
-        <div>Form</div>
+        <div className="flex justify-between px-4">
+            <div className="md:w-2/4 w-full">
+                Fields Side
+                <InvoiceForm/>
+            </div>
+            <div>
+                Tables Side
+                <AdditionalCostTable />
+                <GSTDetailsTable />
+            </div>
+        </div>
     )
 }
 export default Invoice;
