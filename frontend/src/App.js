2b71bf11a0dad830d9168f573811682247141928
raamizz <rameezebrahim@transpost.co>
2025-07-02 12:41:23 +0530

    New Front end pushed

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import InvoiceForm from './components/InvoiceForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <InvoiceForm />
+    </div>
+  );
+}
+
+export default App;
