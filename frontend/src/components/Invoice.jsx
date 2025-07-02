0ff7c9b10169ab062d9846d5db2819969d145dec
raamizz <rameezebrahim@transpost.co>
2025-07-02 13:22:09 +0530

    Layout

--- frontend/src/components/Invoice.jsx
+++ frontend/src/components/Invoice.jsx
@@ -4,24 +4,34 @@ import GSTDetailsTable from "./GSTDetailsTable";
 
 const Invoice = () => {
   return (
-    <div className="flex justify-between px-4">
+    <div className="flex justify-between px-4 gap-1">
       <div className="md:w-2/4 w-full">
         Fields Side
-        
         <InvoiceForm />
       </div>
-      <div>
+      <div className="md:w-2/4 w-full">
         Tables Side
         <div className="flex flex-col col-span-2">
-        <label className="font-medium mb-1">GST Treatment (Maritime) <span className="text-red-500">*</span></label>
-        <div className="flex flex-wrap gap-4 mt-1">
-          <label className="flex items-center"><input type="radio" name="gstTreatment" className="mr-1" /> CGST+SGST</label>
-          <label className="flex items-center"><input type="radio" name="gstTreatment" className="mr-1" /> Export/Zero-Rated</label>
-          <label className="flex items-center"><input type="radio" name="gstTreatment" className="mr-1" /> Domestic (IGST)</label>
+          <label className="font-medium mb-1">
+            GST Treatment (Maritime) <span className="text-red-500">*</span>
+          </label>
+          <div className="flex flex-wrap gap-4 mt-1">
+            <label className="flex items-center">
+              <input type="radio" name="gstTreatment" className="mr-1" />{" "}
+              CGST+SGST
+            </label>
+            <label className="flex items-center">
+              <input type="radio" name="gstTreatment" className="mr-1" />{" "}
+              Export/Zero-Rated
+            </label>
+            <label className="flex items-center">
+              <input type="radio" name="gstTreatment" className="mr-1" />{" "}
+              Domestic (IGST)
+            </label>
+          </div>
         </div>
-      </div>
-        <AdditionalCostTable />
         <GSTDetailsTable />
+        <AdditionalCostTable />
       </div>
     </div>
   );
